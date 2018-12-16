617bbde6c552f31cc9a183fb9b72d42a1ecae346
Calvin Kern <ckern@pixio.com>
2018-12-15 17:50:36 -0700

    Update manifest and added libraries

---
+++ app/src/main/java/com/seakernel/android/scoreapp/ScoreApp.kt
@@ -0,0 +1,22 @@
+package com.seakernel.android.scoreapp
+
+import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
+import timber.log.Timber
+
+/**
+ * Created by Calvin on 12/15/18.
+ * Copyright © 2018 SeaKernel. All rights reserved.
+ */
+class ScoreApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+//            Timber.plant(Timber.asTree()) // No production logging yet
+        }
+    }
+}
