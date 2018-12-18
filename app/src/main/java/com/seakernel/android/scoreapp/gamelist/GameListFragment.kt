7ddb92fcded74d1091aa315e4fe0ac9b9610feac
Calvin Kern <calvinmkern@gmail.com>
2018-12-17 21:39:16 -0700

    Setup recycler view for game list with mobius

--- app/src/main/java/com/seakernel/android/scoreapp/gamelist/GameListFragment.kt
+++ app/src/main/java/com/seakernel/android/scoreapp/gamelist/GameListFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.seakernel.android.scoreapp.R
+import com.seakernel.android.scoreapp.data.Game
 import com.seakernel.android.scoreapp.gamelist.ListModel.Companion.update
 import com.spotify.mobius.Connection
+import com.spotify.mobius.First
 import com.spotify.mobius.Mobius
 import com.spotify.mobius.android.MobiusAndroid
 import com.spotify.mobius.functions.Consumer
 import kotlinx.android.synthetic.main.fragment_game_list.*
+import kotlinx.android.synthetic.main.holder_game_list.view.*
 
 /**
  * Created by Calvin on 12/15/18.
@@ -20,7 +25,7 @@ import kotlinx.android.synthetic.main.fragment_game_list.*
  */
 class GameListFragment : Fragment() {
 
-    private val loop = Mobius.loop(::update, ::effectHandler)
+    private val loop = Mobius.loop(::update, ::effectHandler).init(::initMobius)
     private val controller = MobiusAndroid.controller(loop, ListModel.createDefault())
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -54,22 +59,26 @@ class GameListFragment : Fragment() {
 
     // Mobius functions
 
+    private fun initMobius(model: ListModel): First<ListModel, ListEffect> {
+        return First.first(model, setOf(FetchData))
+    }
+
     private fun connectViews(eventConsumer: Consumer<ListEvent>): Connection<ListModel> {
         // Send events to the consumer when the button is pressed
         fab.setOnClickListener { _ ->
-            // TODO: Show screen to add a new game
             eventConsumer.accept(AddGameClicked)
         }
 
         return object : Connection<ListModel> {
             override fun accept(model: ListModel) {
                 // This will be called whenever there is a new model
-                // TODO: Update adapter with list and eventConsumer
+                gameRecycler.swapAdapter(GameListAdapter(model.gameList, eventConsumer), true)
             }
 
             override fun dispose() {
                 // Don't forget to remove listeners when the UI is disconnected
                 fab.setOnClickListener(null)
+                gameRecycler.swapAdapter(null, true)
             }
         }
     }
@@ -98,5 +107,48 @@ class GameListFragment : Fragment() {
         fun newInstance() : GameListFragment {
             return GameListFragment()
         }
+    }
+}
+
+private class GameListAdapter(private val gameList: List<Game>, private val eventConsumer: Consumer<ListEvent>) : RecyclerView.Adapter<GameListViewHolder>() {
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(GameListViewHolder.RESOURCE_ID, parent, false)
+        return GameListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return gameList.count()
+    }
+
+    override fun onBindViewHolder(holder: GameListViewHolder, position: Int) {
+        holder.bind(gameList[position], eventConsumer)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return gameList[position].id
+    }
+}
+
+private class GameListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val nameHolder: TextView by lazy { itemView.gameNameHolder }
+    val dateHolder: TextView by lazy { itemView.gameDateHolder }
+    val playersHolder: TextView by lazy { itemView.gamePlayersHolder }
+
+    fun bind(game: Game, eventConsumer: Consumer<ListEvent>) {
+        nameHolder.text = game.name
+        dateHolder.text = game.lastPlayedAt
+        playersHolder.text = itemView.context.getString(R.string.playersHolder, game.players.size)
+
+        itemView.setOnClickListener { eventConsumer.accept(GameRowClicked(game.id)) }
+    }
+
+    companion object {
+        const val RESOURCE_ID = R.layout.holder_game_list
     }
 }
