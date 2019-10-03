3b551291da22e932c6c618797c490fe7385e5622
Calvin Kern <calvinmkern@gmail.com>
2019-10-02 21:13:20 -0600

    Add visiblity helper method

--- app/src/main/java/com/seakernel/android/scoreapp/utility/UiExtensions.kt
+++ app/src/main/java/com/seakernel/android/scoreapp/utility/UiExtensions.kt
@@ -1,5 +1,6 @@
 package com.seakernel.android.scoreapp.utility
 
+import android.view.View
 import android.widget.CompoundButton
 import androidx.appcompat.widget.AppCompatCheckBox
 
@@ -7,4 +8,8 @@ fun AppCompatCheckBox.isCheckedSafe(checked: Boolean, listener: CompoundButton.O
     this.setOnCheckedChangeListener(null)
     isChecked = checked
     this.setOnCheckedChangeListener(listener)
+}
+
+fun View.setVisible(visible: Boolean = true) {
+    this.visibility = if (visible) View.VISIBLE else View.GONE
 }
